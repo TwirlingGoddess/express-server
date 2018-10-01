cc610ad964a322d381cb6d83044aed078c34ff9a
Lee Graham <graham.la3@gmail.com>
2018-10-01 00:13:40 -0600

    Implement middleware

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('hello world');
-});
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
+// app.use(urlLogger)
+app.use(express.static('public'))
 
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
+});
+
+app.get('/', (request, response) => {
+  // response.send('hello world');
+});
+
+app.get('/json', urlLogger, timeLogger, (request, response) => {
+  response.status(200).json({"name": "LeeLee"});
 });
